776f21221b16d488aff774805c40eed28abeb446
DylanTse <dylan.tse.1011@gmail.com>
2022-10-02 09:39:49 -0500

    main fronted sections done

--- distledger/src/components/joinDatabase/JoinDatabase.jsx
+++ distledger/src/components/joinDatabase/JoinDatabase.jsx
@@ -5,13 +5,15 @@ export default function JoinDatabase() {
         <div className="join">
             <div className="left">
                 <h1>Join Database</h1>
-                <h4>
+                <p>
                 Join the database that the other party has set up here. Any changes to the database will notify
                 everyone with access to it. Enter a valid unique ID in order to be added to a database. 
-                </h4>
+                </p>
                 <textarea maxlength="100" cols="100" placeholder="Enter Database's Unique User ID"></textarea>
                 <br/>
                 <button>Request Access</button>
+                <p>Current Databases</p>
+                <p>Divide this into archived and active databases until order finished</p>
             </div>
         </div>
     )
