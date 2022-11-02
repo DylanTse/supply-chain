a7d17137df23d64b50e3d0150caf2e1773ff07e9
Kelly <kellyduong@utexas.edu>
2022-11-01 20:26:58 -0500

    Routing of Table

--- distledger/src/App.jsx
+++ distledger/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./app.scss"
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import JoinDatabase from './components/joinDatabase/JoinDatabase';
 import FormDatabase from './components/formDatabase/FormDatabase';
 import Header from './components/header/Header';
@@ -8,14 +8,15 @@ import TransactionPage from './components/TransactionPage/TransactionPage';
 
 function App() {
   return (
-    <div className="app">
-      <div className="section">
-        <Header/>
-        <JoinDatabase/>
-        <FormDatabase/>
-        <TransactionPage/>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Header/>} />
+        <Route path='/Join' element={<JoinDatabase />} />
+        <Route path='/Login' element={<FormDatabase />} />
+        <Route path= '/TransactionPage' element={<TransactionPage/>} />
+        {/* <Route path='/' element={<Home />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
