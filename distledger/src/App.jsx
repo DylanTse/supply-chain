c5ef8980d484547ac58c4dd7806760f5a6155cb4
Kelly <kellyduong@utexas.edu>
2022-11-10 19:11:20 -0600

    Table Header

--- distledger/src/App.jsx
+++ distledger/src/App.jsx
@@ -5,6 +5,7 @@ import JoinDatabase from './components/joinDatabase/JoinDatabase';
 import FormDatabase from './components/formDatabase/FormDatabase';
 import Header from './components/header/Header';
 import TransactionPage from './components/TransactionPage/TransactionPage';
+import TransactionHeader from './components/TransactionPage/TransactionHeader/TransactionHeader';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/Join' element={<JoinDatabase />} />
         <Route path='/Login' element={<FormDatabase />} />
         <Route path= '/TransactionPage' element={<TransactionPage/>} />
+        <Route path='/headertest' element = {<TransactionHeader />} />
         {/* <Route path='/' element={<Home />} /> */}
       </Routes>
     </Router>
--- distledger/src/components/TransactionPage/TransactionPage.jsx
+++ distledger/src/components/TransactionPage/TransactionPage.jsx
@@ -1,4 +1,3 @@
-import "./TransactionPage.scss"
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,29 +7,30 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(date, time, status, id, amount, purchaser) {
+  return { date, time, status, id, amount, purchaser };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  /* createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Gingerbread', 356, 16.0, 49, 3.9), */
 ];
 
-export default function BasicTable() {
+export default function TransactionPage() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer component={Paper} class="Main-Table">
+      <Table title='Transaction' sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="center">Date</TableCell>
+            <TableCell align="center">Time</TableCell>
+            <TableCell align="center">Status</TableCell>
+            <TableCell align="center">ID</TableCell>
+            <TableCell align="center">Amount</TableCell>
+            <TableCell align="center">Purchaser</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -42,10 +42,11 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.date}</TableCell>
+              <TableCell align="right">{row.time}</TableCell>
+              <TableCell align="right">{row.id}</TableCell>
+              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell align="right">{row.purchaser}</TableCell>
             </TableRow>
           ))}
         </TableBody>
